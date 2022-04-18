16749f2c14af81aa2ad5b2527d181c3c723daa75
Hyxogen <8938732+Hyxogen@users.noreply.github.com>
2022-04-18 09:43:47 +0200

    Make * also match no character

--- src/commander/new_pattern.c
+++ src/commander/new_pattern.c
@@ -14,38 +14,11 @@ static int
 	return (!!node->chars[(size_t) ch]);
 }
 
-static int
-	_pattern_match(const char *str, t_pattern_node *node)
-{
-	int	cmp;
-
-	if (*str && node && _is_match(*str, node))
-	{
-		if (node->infinite)
-		{
-			cmp = _pattern_match(str + 1, node);
-			if (cmp)
-				return (1);
-		}
-		return (_pattern_match(str + 1, node->next));
-	}
-	if (*str)
-		return (0);
-	if (!node)
-		return (1);
-	while (node)
-	{
-		if (!node->infinite)
-			return (0);
-		node = node->next;
-	}
-	return (1);
-}
-
+/* TODO rename node->infinite to something more appropriate */
 void
 	pattern_debug_print_node(t_pattern_node *node)
 {
-	int	index;
+	unsigned int	index;
 
 	index = 1;
 	ft_fprintf(STDERR_FILENO, "[");
@@ -67,6 +40,33 @@ void
 		head = head->next;
 	}
 	ft_fprintf(STDERR_FILENO, "\n");
+}
+
+static int
+	_pattern_match(const char *str, t_pattern_node *node)
+{
+	if (*str && node) {
+		if (_is_match(*str, node))
+		{
+			if (node->infinite && _pattern_match(str + 1, node))
+				return (1);
+			if (_pattern_match(str + 1, node->next))
+				return (1);
+		}
+		if (node->infinite)
+			return (_pattern_match(str, node->next));
+	}
+	if (*str)
+		return (0);
+	if (!node)
+		return (1);
+	while (node)
+	{
+		if (!node->infinite)
+			return (0);
+		node = node->next;
+	}
+	return (1);
 }
 
 /* Returns 1 on match */
