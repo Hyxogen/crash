ed48fcc4acb124fa1fcf90cb7648f229e04180aa
Hyxogen <8938732+Hyxogen@users.noreply.github.com>
2022-05-06 09:34:28 +0200

    Norminize condition.c

--- src/commander/condition.c
+++ src/commander/condition.c
@@ -1,49 +1,48 @@
 #include "commander.h"
+
 #include "minishell.h"
 #include "memory.h"
 #include <libft.h>
 #include <string.h>
 
 static pid_t
-	cm_elif_clause(const t_snode *ifnode, const int io[3])
+	cm_elif_clause(const t_snode *node, const int io[3])
 {
 	int	statement_ret;
 	int	body_ret;
 
-	sh_assert(ifnode->type == sx_if_clause);
-	statement_ret = commandeer(ifnode->childs[0], io);
+	sh_assert(node->type == sx_if_clause);
+	statement_ret = commandeer(node->childs[0], io);
 	body_ret = 0;
 	if (!statement_ret)
-		body_ret = commandeer(ifnode->childs[1], io);
-	else if (ifnode->childs_size >= 3
-		&& ifnode->childs[2]->type == sx_if_clause)
-		body_ret = cm_if_clause(ifnode->childs[2], io);
-	else if (ifnode->childs_size >= 3)
-		return (cm_convert_retcode(commandeer(ifnode->childs[2], io)));
+		body_ret = commandeer(node->childs[1], io);
+	else if (node->childs_size >= 3
+		&& node->childs[2]->type == sx_if_clause)
+		body_ret = cm_if_clause(node->childs[2], io);
+	else if (node->childs_size >= 3)
+		return (cm_convert_retcode(commandeer(node->childs[2], io)));
 	return (cm_convert_retcode(body_ret));
 }
 
-/* TODO setup redirects */
 pid_t
-	cm_if_clause(const t_snode *ifnode, const int io[3])
+	cm_if_clause(const t_snode *node, const int io[3])
 {
 	int	old_io[3];
 	int	statement_ret;
 	int	body_ret;
 
-	sh_assert(ifnode->type == sx_if_clause);
-	command_setup_internal_redirects(ifnode->childs[ifnode->childs_size - 1], io, old_io);
-	// _cm_setup_builtin_redirects(ifnode->childs[ifnode->childs_size - 1], if_io);
-	statement_ret = commandeer(ifnode->childs[0], sh()->io);
+	sh_assert(node->type == sx_if_clause);
+	command_setup_internal_redirects(node->childs[node->childs_size - 1],
+		io, old_io);
+	statement_ret = commandeer(node->childs[0], sh()->io);
 	body_ret = 0;
 	if (!statement_ret)
-		body_ret = commandeer(ifnode->childs[1], sh()->io);
-	else if (ifnode->childs_size >= 3
-		&& ifnode->childs[2]->type == sx_if_clause)
-		body_ret = cm_elif_clause(ifnode->childs[2], sh()->io);
-	else if (ifnode->childs_size >= 3)
-		body_ret = commandeer(ifnode->childs[2], sh()->io);
-	// cm_close_nstd_nred(io, sh()->io);
+		body_ret = commandeer(node->childs[1], sh()->io);
+	else if (node->childs_size >= 3
+		&& node->childs[2]->type == sx_if_clause)
+		body_ret = cm_elif_clause(node->childs[2], sh()->io);
+	else if (node->childs_size >= 3)
+		body_ret = commandeer(node->childs[2], sh()->io);
 	command_restore_internal_redirects(io, old_io);
 	return (cm_convert_retcode(body_ret));
 }
@@ -66,26 +65,15 @@ static int
 	return (!match);
 }
 
-pid_t
-	cm_case_clause(const t_snode *node, const int io[3])
+static int
+	execute_case_clause(const t_snode *node, char *lhs, size_t clauses)
 {
-	size_t	clauses;
-	size_t	index;
-	char	*lhs;
-	int		old_io[3];
-	int		cmp;
 	int		rc;
+	int		cmp;
+	size_t	index;
 
-	rc = 0;
-	sh_assert(node->type == sx_case_clause);
-	clauses = node->childs_size - 1;
-	lhs = cm_expand_str(&node->token, NULL, ' ');
-	if (!lhs)
-		return (cm_convert_retcode(1));
 	index = 0;
-	// ft_memcpy(case_io, io, sizeof(int) * 3);
-	// _cm_setup_builtin_redirects(node->childs[node->childs_size - 1], case_io);
-	command_setup_internal_redirects(node->childs[node->childs_size - 1], io, old_io);
+	rc = 0;
 	while (index < clauses)
 	{
 		cmp = _cm_strlst_cmp(lhs, &node->childs[index]->token);
@@ -96,12 +84,30 @@ pid_t
 		}
 		else if (cmp < 0)
 		{
-			rc =  1;
+			rc = 1;
 			break ;
 		}
 		index++;
 	}
-	// cm_close_nstd_nred(io, case_io);
+	return (rc);
+}
+
+pid_t
+	cm_case_clause(const t_snode *node, const int io[3])
+{
+	size_t	clauses;
+	char	*lhs;
+	int		old_io[3];
+	int		rc;
+
+	sh_assert(node->type == sx_case_clause);
+	clauses = node->childs_size - 1;
+	lhs = cm_expand_str(&node->token, NULL, ' ');
+	if (!lhs)
+		return (cm_convert_retcode(1));
+	command_setup_internal_redirects(node->childs[node->childs_size - 1],
+		io, old_io);
+	rc = execute_case_clause(node, lhs, clauses);
 	command_restore_internal_redirects(io, old_io);
 	return (cm_convert_retcode(rc));
 }
